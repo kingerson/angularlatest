3590d15c8870a189dda9d525171c1866f3f38d48
Carlos Salvador <csalvador963@outlook.com>
2020-03-18 22:33:14 -0500

    add integration Service

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { HttpErrorResponse } from '@angular/common/http';
+import { ExternalService } from '../services/external.service';
+import { PostTest } from '../models/external-model';
 
 @Component({
   selector: 'app-admin',
@@ -6,9 +9,77 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private externalService: ExternalService
+  ) {}
 
   ngOnInit(): void {
     console.log('admin')
   }
+
+  GetUserWS(){
+    this.externalService.getUser(1).subscribe((response) => {
+      console.log("Response Get --> ", response)
+    },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      });
+  }
+
+  GetListWS(){
+    this.externalService.getListPost().subscribe((response) => {
+      console.log("Response List --> ", response)
+    },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      });
+  }
+
+  CreatePostWS() {
+    var model = new PostTest();
+        model.id = 1;
+        model.title = "foo";
+        model.body = "bar";
+    
+    this.externalService.postCreate(model).subscribe((response) => {
+      console.log("Response Post --> ", response)
+    },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      });
+  }
+
+  UpdateostWS() {
+    var model = new PostTest();
+        model.id = 1;
+        model.title = "foo";
+        model.body = "bar";
+    
+    this.externalService.updateUser(1,model).subscribe((response) => {
+      console.log("Response Update --> ", response)
+    },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      });
+  }
+
+  DeleteUserWS() {
+    this.externalService.deleteUser(1).subscribe((response) => {
+      console.log("Response Delete --> ", response)
+    },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      });
+  }
+
+  FilterUserWS() {
+    var filter = { userId: 1 };
+    this.externalService.filterUser(filter).subscribe((response) => {
+      console.log("Response Filter --> ", response)
+    },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      });
+  }
+
 }
---
+++ src/app/features/models/external-model.ts
@@ -0,0 +1,18 @@
+export class ExternalModel {
+}
+
+
+export class UserTest {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+
+}
+
+export class PostTest {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
